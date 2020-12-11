8f2461f9b48526881503930f7d6a3b1f9cc04d3b
Waclaw Bielecki <hardhill@mail.ru>
2020-12-11 10:59:20 +0800

    create, save and delete POI

--- POIApp/DetailActivity.cs
+++ POIApp/DetailActivity.cs
@@ -40,6 +40,46 @@ namespace POIApp
             UpdateUI();
 
         }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_detailview, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.actionSave:
+                    SavePOI();
+                    return true;
+                    
+                    
+            case Resource.Id.actionDelete:
+                    DeletePOI();
+                    return true;
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+
+        private void DeletePOI()
+        {
+            DeletePOIAsync();
+        }
+
+        private void SavePOI()
+        {
+            double tempLatitude = Convert.ToDouble(edtLatitude.Text);
+            double tempLongitude = Convert.ToDouble(edtLongitude.Text);
+            _poi.Name = edtName.Text;
+            _poi.Description = edtDescription.Text;
+            _poi.Address = edtAddress.Text;
+            _poi.Latitude = tempLatitude;
+            _poi.Longitude = tempLongitude;
+            CreateOrUpdatePOIAsync(_poi);
+        }
+
+        
 
         protected void UpdateUI()
         {
@@ -48,6 +88,52 @@ namespace POIApp
             edtDescription.Text = _poi.Description;
             edtLongitude.Text = _poi.Longitude.ToString();
             edtLatitude.Text = _poi.Latitude.ToString();
+        }
+
+        private async void CreateOrUpdatePOIAsync(PointOfInterest poi)
+        {
+            POIService service = new POIService();
+            if (!service.isConnected(this))
+            {
+                Toast toast = Toast.MakeText(this, "Not conntected to internet.Please check your device network settings.", ToastLength.Short);
+                toast.Show();
+                return;
+            }
+            string response = await service.CreateOrUpdatePOIAsync(_poi);
+            if (!string.IsNullOrEmpty(response))
+            {
+                Toast toast = Toast.MakeText(this, String.Format("{0} saved.", _poi.Name), ToastLength.Short);
+                toast.Show();
+                Finish();
+            }
+            else
+            {
+                Toast toast = Toast.MakeText(this, "Something went Wrong!", ToastLength.Short);
+                toast.Show();
+            }
+        }
+
+        private async void DeletePOIAsync()
+        {
+            POIService service = new POIService();
+            if (!service.isConnected(this))
+            {
+                Toast toast = Toast.MakeText(this, "Not conntected to internet. Please check your device network settings.", ToastLength.Short);
+                toast.Show();
+                return;
+            }
+            string response = await service.DeletePOIAsync(_poi.Id);
+            if (!string.IsNullOrEmpty(response))
+            {
+                Toast toast = Toast.MakeText(this, String.Format("{0} deleted.", _poi.Name), ToastLength.Short);
+                toast.Show();
+                Finish();
+            }
+            else
+            {
+               Toast toast = Toast.MakeText(this, "Something went Wrong!",ToastLength.Short);
+               toast.Show();
+            }
         }
     }
 }
